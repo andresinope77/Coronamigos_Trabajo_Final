2f3b4c08696d7a9da1b96b248ffee566d21fce15
NoBullShiet <64295997+NoBullShiet@users.noreply.github.com>
2020-04-28 21:04:14 -0500

    Update MainCode.rb

--- MainCode.rb
+++ MainCode.rb
@@ -2,20 +2,50 @@
 #Grupo Coronamigos
 
 class Alumno
-	attr_accessor :dni, :nombre, :apellido, :edad, :genero, :listaTutores
+	attr_accessor :dni, :nombre, :apellido, :edad, :genero, :CS, :RE, :EC, :puntajeFinal, :listaTutores, :resultado
 	def initialize(dni, nombre, apellido, edad, genero)
   	@dni, @nombre, @apellido, @edad, @genero = dni, nombre, apellido, edad, genero
     @listaTutores = Array.new
+		    @CS = 0
+    @RE = 0
+    @EC = 0
+    @puntajeFinal = 0
+    @resultado = "NO INGRESA"
   end
 end
 
 class AlumnoNacional < Alumno
 	attr_accessor :tipo, :promedio2do, :colegio
-  def initialize(dni, nombre, apellido, edad, genero, tipo, promedio2do)
+  	def initialize(dni, nombre, apellido, edad, genero, tipo, promedio2do)
  		super(dni, nombre, apellido, edad, genero)
  		@colegio = "NACIONAL"
  		@tipo = tipo.upcase
  		@promedio2do = promedio2do
+ 	end
+ 	def calificarSocioEconomica
+ 		if tipo == "RURAL"
+ 			return 100
+		elsif tipo == "URBANA"
+ 			return 100
+ 		else
+ 			return 0
+ 		end
+ 	end
+
+ 	def calificarRendimiento
+ 		if promedio2do < 11
+ 			return 0
+		elsif promedio2do < 14
+ 			return 20
+		elsif promedio2do < 16
+ 			return 40
+		elsif promedio2do < 18
+ 			return 60
+		elsif promedio2do < 19
+ 			return 80
+ 		else
+ 			return 100
+ 		end
  	end
 end
 
@@ -27,6 +57,32 @@ class AlumnoParticular < Alumno
  		@pension = pension
  		@puesto2do = puesto2do
  	end
+ 	def calificarSocioEconomica
+ 		if pension <= 200
+ 			return 90
+  	elsif pension <= 400
+  		return 70
+		elsif pension <= 600
+ 			return 50
+ 		else
+ 			return 40
+ 		end
+ 	end
+
+ 	def calificarRendimiento
+ 		if puesto2do < 4
+ 			return 100
+		elsif puesto2do < 6
+ 			return 80
+		elsif puesto2do < 11
+ 			return 60
+		elsif puesto2do < 21
+ 			return 40
+ 		else
+ 			return 0
+ 		end
+ 	end
+
 end
 
 class Tutor
@@ -43,4 +99,25 @@ class Examen
     @listaRespuestasAlumno = Array.new(numeroPregunta)
     @listaRespuestasCorrectas = Array.new(numeroPregunta)
 	end
+  def simularResultados(examen)
+    #lógica para recorrer todo el array y asignale una letra aleatoria
+    n = examen.numeroPregunta - 1
+    for i in 0..n
+      a = ('a'..'d').to_a.sample            #comando para sacar una letra aleatoria a - d (siendo d la respuesta en blanco)
+      examen.listaRespuestasAlumno[i] = a   #se ingresa la letra aleatoria al array
+    end
+    #puts "#{examen.listaRespuestasAlumno}" #prueba de imprimir el array para ver los resultados del aleatorio
+  end
+end
+
+class Ministerio
+  attr_accessor :listaAlumnos, :listaExamenes, :listaIngresantes, :listaNoIngresantes
+	def initialize
+    #Arrays para toda la lógica
+		@listaAlumnos = Array.new
+    @listaExamenes = Array.new
+    @listaIngresantes = Array.new
+    @listaNoIngresantes = Array.new
+	end
+	
 end
